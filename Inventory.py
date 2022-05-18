cf97469e2da572e6e989a3005d64bf8f6ad133da
Joy Kiprotich <joychepkemboi50@gmail.com>
2022-05-18 18:58:26 -0500

    Menu added and integrated to logic
    Menu added in Main.py
    Refined logic

--- Inventory.py
+++ Inventory.py
@@ -1,11 +1,19 @@
 class Inventory:
-    #initialize new inventory that will store items
+    #initialize new inventory that will store items and bin to store most recently deleted item
     def __init__(self):  
         self.inventory = {}
         self.recycle_bin = {}
 
     #create and add items to the inventory
-    def create(self, id, name, description, cost, quantity):
+    def create(self, id):
+        #check if item being created already exists
+        if id in self.inventory:
+            print("Cannot create item, it already exists in inventory")
+            return -1
+        name = input("Item Name: ")
+        description = input("Item Description: ")
+        cost = input("Item Cost: ")
+        quantity = input("Item Quantity: ")
         attributes = {
             "name": name,
             "description": description,
@@ -14,14 +22,18 @@ class Inventory:
         }
         self.inventory[id] = attributes
 
+        return 0
+
     #update current inventory items
     def update(self,id):
-        #check if id provided is valid first
-        if not self.inventory[id]:
-           return -1 
+        #check if item exists first
+        if id not in self.inventory:
+            print("Cannot update item, it does not exist in inventory")
+            return -1 
         print("Item found, input new values only for attributes to be changed else press ENTER")
         for key in self.inventory[id]:
-            new_val = ("Update ",key,": ")
+            print("Update",key,":", end="")
+            new_val = input()
             if new_val:
                 self.inventory[id][key] = new_val
 
@@ -29,9 +41,10 @@ class Inventory:
 
     #delete current items in inventory
     def delete(self, id):
-        #check if id provided is valid first
-        if not self.inventory[id]:
-           return -1 
+        #check if item exists first
+        if id not in self.inventory:
+            print("Cannot delete item, it does not exist in inventory")
+            return -1 
         self.recycle_bin = {
             "id": id,
             "name": self.inventory[id]["name"],
@@ -39,13 +52,32 @@ class Inventory:
             "cost": self.inventory[id]["cost"],
             "quantity": self.inventory[id]["quantity"]
         }
-        self.inventoty.pop(id)
+        self.inventory.pop(id)
+        return 0
     
-   
+    #undelete the most recently deleted item
     def undelete(self):
         id = 0
+        if not self.recycle_bin:
+            print("No recently deleted item")
+            return -1
         for key,value in self.recycle_bin.items():
             if key=="id":
                 id = value
+                self.inventory[id] = {}
             else:
                 self.inventory[id][key] = value
+
+        return 0
+
+    def print_all(self):
+        if not self.inventory:
+            print("Inventory is empty")
+            return -1
+        for key,value in self.inventory.items():
+            print("__",key,"__")
+            for k,v in value.items():
+                print(k,":",v)
+            print("______________")
+
+        return 0
---
+++ Main.py
@@ -0,0 +1,50 @@
+from Inventory import Inventory
+
+def menu():
+   print("To create and add item to inventory, press 1") 
+   print("To update item in inventory, press 2")
+   print("To delete item in inventory, press 3")
+   print("To view all items, press 4")
+   print("To undelte recently deleted item, press 5")
+
+if __name__== "__main__":
+    inv = Inventory()
+    valid_choices= ["1","2","3","4","5"]
+    invalid_choices = ["q","Q"]
+    success = 0
+    print("****INVENTORY****")
+    start = input("Only press q to quit, anything else to proceed: ")
+
+    while start not in invalid_choices:
+        menu()
+        choice = input("Enter choice: ")
+        while choice not in valid_choices:
+            print("Wrong choice!")
+            menu()
+            choice = input("Enter choice: ")
+        #get Item ID from user only if undeleted is not selected. Make sure ID is input, unique ID is essential for working of the Inventory system
+        if choice in valid_choices[:3]:
+            id = input("Item Id: ")
+            while not id:
+                print("ID Needed!")
+                id = input("Item Id: ")
+
+        if choice == "1":
+            success = inv.create(id)
+        elif choice == "2":
+            success = inv.update(id)
+        elif choice == "3":
+            success = inv.delete(id)
+        elif choice == "4":
+            success = inv.print_all()
+        else:
+            success = inv.undelete()
+
+        if success == -1:
+            print("Process not successful")
+        else:
+            print("Process successful")
+
+        start = input("Only press q to quit, anything else to proceed: ")
+
+    print("****CLOSING INVENTORY****")
